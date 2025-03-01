6f2e73305f005d4dae7e7c04aeee990ef0ac72d3
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2025-03-01 18:31:57 +0100

    introduction split into 2 parts - mostly OK

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,36 +1,35 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'UE12 - Introduction',
-			social: {
-				github: 'https://github.com/ue12-p25/intro',
-			},
-			sidebar: [
-				{
-					label: 'Introduction',
-					items: [
-						{ label: 'Présentation', link: '/0-00-presentation-ue12-ue22' },
-					],
-				},
-				{
-					label: 'Installation et configuration',
-					items: [
-						{ label: 'Installations', link: '/1-01-installations' },
-						{ label: 'Cloner le cours', link: '/1-02-clone-and-play' },
-					],
-				},
-				{
-					label: 'Outils de développement',
-					items: [
-						{ label: 'Python', link: '/2-03-python' },
-					],
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "UE12 - Introduction",
+      social: {
+        github: "https://github.com/ue12-p25/intro",
+      },
+      sidebar: [
+        {
+          label: "Introduction",
+          items: [
+            { label: "<tl;dr>", link: "/0-1-presentation-ue12-ue22" },
+            { label: "Version longue", link: "/0-2-presentation-longue" },
+          ],
+        },
+        {
+          label: "Installation et configuration",
+          items: [
+            { label: "Installations", link: "/1-01-installations" },
+            { label: "Cloner le cours", link: "/1-02-clone-and-play" },
+          ],
+        },
+        {
+          label: "Outils de développement",
+          items: [{ label: "Python", link: "/2-03-python" }],
+        },
+      ],
+    }),
+  ],
 });

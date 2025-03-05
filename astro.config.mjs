c4e564d4309449a3d80add439a0eb09013f66cd1
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2025-03-05 16:39:53 +0100

    adding a base: 'intro' and tweaking index.mdx accordingly

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import starlightThemeRapide from 'starlight-theme-rapide'
 
 // https://astro.build/config
 export default defineConfig({
+  base: 'intro',
   integrations: [
     starlight({
       title: "UE12/UE22 - Introduction",
